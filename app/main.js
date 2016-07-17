e794986b09530124f8e8db27e3670d1c737adba9
lengyeljanos <polish1987@gmail.com>
2016-07-17 12:07:02 +0200

    remove js and map files from source

--- app/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var todo_list_component_1 = require('./components/todo-list/todo.list.component');
-var todo_store_1 = require('./store/todo.store');
-platform_browser_dynamic_1.bootstrap(todo_list_component_1.TodoList, [todo_store_1.TodoListStore]);
-//# sourceMappingURL=main.js.map
